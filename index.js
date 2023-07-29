e53b9b47ff2f591f608c716acd3810ce36a5421e
illyalane <lane_illya@yahoo.com>
2023-07-28 21:23:19 -0400

    completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+function saturdayFun(act = "roller-skate"){
+
+    return (`This Saturday, I want to ${act}!`);
+}
+saturdayFun();
+saturdayFun("bathe the dog");
+
+function mondayWork(work = "go to the office"){
+    return `This Monday, I will ${work}.`;
+}
+
+function wrapAdjective(pray = "*"){
+
+    const innerFunction = function(hope = "special"){
+        return `You are ${pray}${hope}${pray}!`;
+    };
+        return innerFunction;
+}
+
+wrapAdjective("%")("a dedicated programmer");
